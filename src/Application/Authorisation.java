27af8344288bdc95020e18da021316af05fe0a26
Adanos <gothicgame29@gmail.com>
2021-02-27 19:44:04 +0700

    fix authorisation to Database

--- src/Application/Authorisation.java
+++ src/Application/Authorisation.java
@@ -1,27 +1,46 @@
 package Application;
 
+import Utils.ConnectionManager;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Authorisation
 {
     @FXML Button signButton;
+    @FXML TextField loginField;
+    @FXML TextField passField;
 
     public void Sign() throws IOException
     {
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setLocation(getClass().getResource("Store.fxml"));
+        ConnectionManager manager = new ConnectionManager();
+        manager.GetDBConnect();
+        Init();
+    }
 
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        Stage stage = (Stage) signButton.getScene().getWindow();
-        stage.close();
+    public void Init()
+    {
+        signButton.setOnAction(actionEvent ->
+        {
+            try
+            {
+                ResultSet result = ConnectionManager.statement.executeQuery
+                        ("SELECT id FROM user WHERE " +
+                                "login = \'" + loginField.getText() + "\' AND" + " " +
+                                "password = \'" + passField.getText() + "\'");
 
-        stage.setTitle("Store");
-        stage.setScene(scene);
-        stage.show();
+                if (result.next())
+                    System.out.println("Your account is Director: " + result.getInt("id"));
+                else
+                    System.out.println("Not found account");
+            }
+            catch (SQLException err) { err.printStackTrace(); }
+        });
     }
 }
--- src/Application/AuthorisationMain.java
+++ src/Application/AuthorisationMain.java
@@ -1,32 +1,27 @@
 package Application;
 
-import Utils.ConnectionManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.sql.SQLException;
-
+/*
+This is Entry point to Database. Don't modify this code block!!!
+*/
 public class AuthorisationMain extends Application
 {
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        Parent root = FXMLLoader.load(getClass().getResource("Registration.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Authorisation.fxml"));
         primaryStage.setTitle("Store DataBase");
         primaryStage.setScene(new Scene(root, 432, 259));
         primaryStage.show();
     }
 
-    public static void main(String[] args) throws SQLException
+    public static void main(String[] args)
     {
-        ConnectionManager.GetDBConnect();
-
         launch(args);
-
-        if (ConnectionManager.GetDBConnect().isClosed())
-            System.out.println("Connection to DB is closed.");
     }
 }
